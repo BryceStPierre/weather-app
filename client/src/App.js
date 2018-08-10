1e08d67517fb2418921d578ace76d8915319aa89
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-08-09 22:06:25 -0400

    Updated App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, NavbarBrand, Row, Col, Jumbotron, Button, InputGroup, FormGroup, Input, InputGroupAddon } from 'reactstrap';
+
 import './App.css';
 
+const options = [
+  'Toronto',
+  'Vancouver',
+  'Montreal',
+  'Halifax'
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      newCity: '',
+      city: null
+    }
+  }
+
+  handleNewCityChange = (e) => {
+    this.setState({ newCity: e.target.value });
+    console.log(e.target.value);
+  }
+
+  handleChangeCity = (e) => {
+    this.setState({ city: e.target.value });
+    console.log(e.target.value);
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ textAlign: 'center' }}>
+        <Navbar color="dark" dark>
+          <NavbarBrand href="/">MyWeather</NavbarBrand>
+        </Navbar>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1 className="display-3">MyWeather</h1>
+              <p className="lead">The current weather for your favorite cities!</p>
+
+                <InputGroup style={{ width: '275px', margin: '0 auto' }}>
+                  <Input 
+                    value={this.state.newCity}
+                    onChange={this.handleNewCityChange}
+                    placeholder="City name..." 
+                  />
+                  <InputGroupAddon addonType="append">
+                    <Button color="primary">Add City</Button>
+                  </InputGroupAddon>
+                </InputGroup>
+
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1 className="display-5">Current Weather</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <FormGroup style={{ width: '275px', margin: '0 auto' }}>
+              <Input type="select" onChange={this.handleCity}>
+              { options.length === 0 && <option>No cities added yet.</option> }
+              { options.map(option => <option key={option}>{option}</option>) }
+              </Input>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          
+          </Col>
+        </Row>
       </div>
     );
   }
