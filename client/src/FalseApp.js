caa40c57234eb440e769be085c1d87049a023cd9
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-08-23 19:51:58 -0400

    Updated FalseApp.

--- client/src/FalseApp.js
+++ client/src/FalseApp.js
@@ -23,6 +23,23 @@ class App extends Component {
     }
   }
 
+  handleInputChange = (e) => {
+    this.setState({ newCityName: e.target.value });
+  };
+
+  handleAddCity = () => {
+    fetch('/api/cities', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ city: this.state.newCityName })
+    })
+    .then(res => res.json())
+    .then(res => {
+      this.getCityList();
+      this.setState({ newCityName: '' });
+    });
+  };
+
   getCityList = () => {
     fetch('/api/cities')
     .then(res => res.json())
@@ -43,7 +60,23 @@ class App extends Component {
           <NavbarBrand href="/">MyWeather</NavbarBrand>
         </Navbar>
         <Row>
-          <Col></Col>
+          <Col>
+            <Jumbotron>
+              <h1 className="display-3">MyWeather</h1>
+              <p className="lead">The current weather for your favorite cities!</p>
+              <InputGroup>
+                <Input
+                  placeholder="New city name..."
+                  value={this.state.newCityName}
+                  onChange={this.handleInputChange}
+                />
+                <InputGroupAddon addonType="append">
+                  <Button color="primary" onClick={this.handleAddCity}>Add City</Button>
+                </InputGroupAddon>
+              </InputGroup>
+            </Jumbotron>
+          
+          </Col>
         </Row>
         <Row>
           <Col></Col>
